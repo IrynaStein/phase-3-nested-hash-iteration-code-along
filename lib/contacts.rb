9e6f6b90daeb1a1f3fcf278179c2e4adef3564c0
Iryna Stein <isophiestein@gmail.com>
2021-07-14 14:11:16 -0600

    commit tree 3951b606869aa7ece5273a8a96f9464a7528bf2f

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,23 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+      contacts.each do |person, contact_details_hash|
+        if person == "Freddy Mercury" 
+          contact_details_hash.each do |attribute, data|
+            if attribute == :favorite_ice_cream_flavors
+              data.delete_if {|ice_cream| ice_cream == "strawberry"}
+            end
+          end
+        end
+      end
 end
+
+# def remove_strawberry(contacts)
+#   contacts.each do |person, contact_details_hash|
+#     next unless person == 'Freddy Mercury'
+
+#     contact_details_hash.each do |attribute, data|
+#       data.delete_if { |ice_cream| ice_cream == 'strawberry' } if attribute == :favorite_ice_cream_flavors
+#     end
+#   end
+# end
